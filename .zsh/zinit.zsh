1ce0a84867fed61a5fa5af1cf698b501aa6ba7ee
Shunta Komatsu <shuntak217@gmail.com>
2020-10-06 01:41:24 +0900

    Add plugins

--- .zsh/zinit.zsh
+++ .zsh/zinit.zsh
@@ -1,7 +1,9 @@
 source ~/.zinit/bin/zinit.zsh
 
 zinit ice from gh-r as program
+zinit light b4b4r07/emoji-cli
 zinit light djui/alias-tips
+zinit light mollifier/anyframe
 zinit light mollifier/cd-gitroot
 zinit light reegnz/jq-zsh-plugin
 zinit light romkatv/powerlevel10k
